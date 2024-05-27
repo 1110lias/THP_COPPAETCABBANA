1b18b800235fe167246776f66e2fbd4fff6eec7f
valentin-orrit <vorrit@gmail.com>
2024-05-27 17:10:30 +0200

    modified tailwind config and import fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './public/*.html',
     './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Sansita', ...defaultTheme.fontFamily.sans],
+        'serif': ['Scope One', ...defaultTheme.fontFamily.serif],
+        'mono': ['Thasadith', ...defaultTheme.fontFamily.mono]
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ]
 }
